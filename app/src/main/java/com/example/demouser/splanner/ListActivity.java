5d1fe6cfe7b8c7ab6dda298814a667d85a4ffa2e
Ranjani Ramanathan <ranjarama4@gmail.com>
2019-01-16 11:54:10 -0500

    Remove .idea and .iml files.

--- app/src/main/java/com/example/demouser/splanner/ListActivity.java
+++ app/src/main/java/com/example/demouser/splanner/ListActivity.java
@@ -4,10 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ListActivity extends AppCompatActivity {
-
+private SearchActivity activeLlist;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+
     }
 }
