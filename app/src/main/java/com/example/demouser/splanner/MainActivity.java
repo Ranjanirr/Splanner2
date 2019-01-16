6e921b2b64f59f874a5f92aabdcf5d8705441900
yu24l <yu24l@mtholyoke.edu>
2019-01-16 10:29:42 -0500

    Parsing Spring 2019 course data, display a list of course on the search page

--- app/src/main/java/com/example/demouser/splanner/MainActivity.java
+++ app/src/main/java/com/example/demouser/splanner/MainActivity.java
@@ -1,18 +1,52 @@
 package com.example.demouser.splanner;
 
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+//    // contains CS classes from Spring 2019 data
+//    private HashMap<String, Course> courseList = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+//        AssetManager assetManager = getAssets();
+//        try {
+//            InputStream inputStream = assetManager.open("courses.txt");
+//            BufferedReader in  = new BufferedReader(new InputStreamReader(inputStream));
+//            String line;
+//            while((line = in.readLine()) != null){
+//                String[] courseInfo = line.split("\\|");
+//                Course newCourse = new Course();
+//                newCourse.setCourseNumber(courseInfo[0]);
+//                newCourse.setCourseTitle(courseInfo[1]);
+//                newCourse.setMeetingDays(courseInfo[2]);
+//                newCourse.setCourseTime(courseInfo[3]);
+//                newCourse.setCredit(courseInfo[4]);
+//
+//                courseList.put(newCourse.getCourseTitle(), newCourse);
+//            }
+//
+//        } catch (IOException e) {
+//            Toast toast = Toast.makeText(this, "Could not load dictionary", Toast.LENGTH_LONG);
+//            toast.show();
+//        }
 
         Button searchBtn = findViewById(R.id.searchButton);
         searchBtn.setOnClickListener(new View.OnClickListener() {
@@ -38,10 +72,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Course c1 = new Course("1", "Advanced Programming","TT", "1");
-        Course temp1 = new Course("COMSC-109-01", "iDesign Studio",
-                "Monday, Wednesday", "02:55PM - 04:10PM");
-
+        Course temp1 = new Course("COMSC-109-01", "iDesign Studio", "Monday, Wednesday", "02:55PM - 04:10PM", "4");
     }
 
     private void onMyPlans() {
