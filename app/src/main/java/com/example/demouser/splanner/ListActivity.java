caec4a2037ced8151b2af9d3f61dd6411fcb2896
yu24l <yu24l@mtholyoke.edu>
2019-01-17 16:28:01 -0500

    Second try; to pull from master branch

--- app/src/main/java/com/example/demouser/splanner/ListActivity.java
+++ app/src/main/java/com/example/demouser/splanner/ListActivity.java
@@ -2,7 +2,6 @@ package com.example.demouser.splanner;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,8 +16,6 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -31,8 +28,6 @@ public class ListActivity extends AppCompatActivity {
     private ItemsListAdapter courseListAdapter;
 
     private List<Course> myCourseList;
-    private List<Course> plan = new ArrayList<>();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,9 +38,9 @@ public class ListActivity extends AppCompatActivity {
         selectButton = (Button) findViewById(R.id.selectAll);
         listView = (ListView)findViewById(R.id.listview);
 
-        myCourseList = new LinkedList<>();
+//        myCourseList = new LinkedList<>();
+//        initCourseData();
         myCourseList = CoursesData.instance.getSelectedCourses();
-        plan =  CoursesData.instance.getPlan();
 
         courseListAdapter = new ItemsListAdapter(this, myCourseList);
         listView.setAdapter(courseListAdapter);
@@ -72,6 +67,7 @@ public class ListActivity extends AppCompatActivity {
                     }
                     count++;
                 }
+                CoursesData.instance.setSelectedCourses(myCourseList);
 
                 Toast.makeText(ListActivity.this, str, Toast.LENGTH_LONG).show();
                 listView.setAdapter(courseListAdapter);
@@ -84,7 +80,6 @@ public class ListActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 //                Log.i("test", "hhhh?");
-                generateClassPlans();
             }
         });
 
@@ -179,7 +174,7 @@ public class ListActivity extends AppCompatActivity {
                     boolean newState = !list.get(position).isChecked();
                     list.get(position).setChecked(newState);
                     Toast.makeText(getApplicationContext(),
-                            "You selected: \n"+itemStr,
+                            itemStr + "setOnClickListener\nchecked: " + newState,
                             Toast.LENGTH_LONG).show();
                 }
             });
@@ -189,39 +184,124 @@ public class ListActivity extends AppCompatActivity {
             return rowView;
         }
     }
+    //    private SearchActivity activity;
+//    private ListView listView;
+//    private Button btnLookup;
+//
+//    private ArrayList<Course> myCourseList = new ArrayList<>();
+//    private ArrayList<Plan> myPlanList = new ArrayList<>();
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_list);
+//
+//        btnLookup = (Button) findViewById(R.id.button);
+//        final LinearLayout classLayout = (LinearLayout) findViewById(R.id.linny);
+//        final TextView classView = findViewById(R.id.textView7);
+//
+//        btnLookup.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                String str = "Check items:\n";
+//
+//                for (int i = 0; i < CourseList.instance.getCourses().size(); i++) {
+//                    if (CourseList.instance.getCourses().get(i).isChecked()) {
+//                        classView.setText(classView.getText() + CourseList.instance.getCourses().get(i).getCourseTitle() + "\n");
+//
+//                    }
+//
+//                    Toast.makeText(ListActivity.this, str, Toast.LENGTH_LONG).show();
+//                }
+//            }
+//        });
+//    }
 
-    private void generateClassPlans() {
-        ArrayList<Course> currentPlan = new ArrayList<>();
-        currentPlan.clear();
+//        initCourseData();
 
-        for (Course c: myCourseList) {
-            if (c.isChecked()) {
-                currentPlan.add(c);
-            }
-        }
-//        Log.i("test", "\n" + currentPlan);
+//        Button generateBtn = findViewById(R.id.generateButton);
+//        generateBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                generateClassPlans(new Plan(), new HashSet<Course>(),0, 0);
+//            }
+//        });
+//
+//        //Log.i("test","conflict? "+ myCourseList.get(0).isConflict(myCourseList.get(1)));
+//    }
 
-        // if there are less than 4 courses
-        // add all the un-overlap courses to plans
-//        if (currentPlan.size() <= 4) {
-//            Log.i("enter", "\n" + currentPlan);
-            for (int i = 0; i < currentPlan.size(); i++) {
-                for (int j = i+1; j < currentPlan.size(); j++) {
-                   if (currentPlan.get(i).isConflict(currentPlan.get(j))) {
-                       currentPlan.remove(currentPlan.get(i));
-                   }
-                }
-            }
-//            Log.i("after", "\n" + currentPlan);
-            plan.clear();
-            plan.addAll(currentPlan);
-//            Log.i("after after", "\n" + currentPlan);
-//            Log.i("after that my plan is", "\n" + plan);
-            Intent intent = new Intent(this, PlansActivity.class);
-            startActivity(intent);
-//        }
-
+    private void initCourseData() {
+        Course temp1 = new Course("COMSC-109-01", "iDesign Studio", "Monday", "02:55PM - 04:10PM", "4");
+        myCourseList.add(temp1);
+        myCourseList.add(new Course("COMSC-132-01", "Engineering for Everyone", "Thursday", "01:30PM - 02:45PM", "4"));
+        myCourseList.add(new Course("COMSC-151AA-01", "CS Prob Solv: Algorithmic Art", "Monday, Wednesday", "02:55PM - 04:10PM", "4"));
+        myCourseList.add(new Course("COMSC-151AAL-01", "CS Prob Solv: Algorithmic Lab", "Friday", "01:30PM - 04:20PM", "4"));
+        myCourseList.add(new Course("COMSC-151AR-01", "CS Prov Solv: Artificl Intel.", "Tuesday, Thursday", "08:30AM - 09:45AM", "4"));
+        myCourseList.add(new Course("COMSC-205-01", "Data Structures", "Tuesday, Thursday", "11:30AM - 12:45PM", "4"));
+        myCourseList.add(new Course("COMSC-226-01", "Engineering Robotic Systems", "Tuesday", "10:00AM - 11:15AM", "4"));
+        myCourseList.add(new Course("COMSC-312-01", "Algorithms", "Monday, Wednesday", "11:00AM - 12:15PM", "4"));
+        myCourseList.add(new Course("COMSC-322-01", "Operating Systems", "Tuesday, Thursday", "11:30AM - 12:45PM", "4"));
+        myCourseList.add(new Course("COMSC-335-01", "Machine Learning", "Tuesday, Thursday", "10:00AM - 11:15AM", "4"));
     }
-}
+//        });
 
+
+
+
+//     }
+//        Log.i("another test", "is conflict? " +myCourseList.get(0).isTimeConflict(myCourseList.get(7)));
+//        Log.i("another test", "course1 start time? " +myCourseList.get(7).getStartTime() +" " + myCourseList.get(7).getEndTime());
+//    }
+
+    // for all selected classes (stored in myCourseList)
+    // choose 4 from them without any time slot overlapped
+    // for now we only aim for generating 2 plans
+    // courseNum: how many courses are there in current course plan
+    // index: marks the startpoint that had been used in myCourseList
+//    private void generateClassPlans(Plan currentPlan, HashSet<Course> candidates, int courseNum, int index) {
+//       if (courseNum == 3) {
+//           myPlanList.add(currentPlan);
+//           Log.i("test", "my current plan length: "+ currentPlan.size());
+//           Log.i("test", "my current plan: "+ currentPlan.toString());
+//           return;
+//       }
+//       else {
+//
+//           // initialize step
+//            if (currentPlan.isEmpty()) {
+//                Course currentCourse = myCourseList.get(index);
+//                currentPlan.add(currentCourse);
+//                for (Course c: myCourseList) {
+//                    if (!c.isConflict(currentCourse)) {
+//                        candidates.add(c);
+//                    }
+//                }
+//            }
+//            // re-edit the hash set
+//            // remove things from the set
+//            else {
+//                // the last item in current plan
+//                Course currentCourse = currentPlan.get(currentPlan.size()-1);
+//                Log.i("another test", "current course : \n"+ currentCourse.toString());
+//                for (Course c: myCourseList) {
+//                    if (c.isConflict(currentCourse)) {
+//                        candidates.remove(c);
+//                    }
+//                }
+//           }
+//
+//           Log.i("another test", "my candidates are: \n"+ candidates.toString() + "\n" +candidates.size() + "\n");
+//
+//           // recursive call for every candidate
+//           for (Course candidate: candidates) {
+//                currentPlan.add(candidate);
+//                generateClassPlans(currentPlan, candidates,courseNum+1, index+1);
+//               // break;
+//                currentPlan.remove(candidate);
+//           }
+//       }
+//    }
+
+}
 
